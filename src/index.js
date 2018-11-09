2832c891ff9026c281013a40e5f0f62c6326e09d
Brandon Chung <brandon.k.chung@gmail.com>
2018-11-09 12:33:39 -0600

    redirect for confession-app

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+if (window.location.href === 'https://bkchu.github.io/confession-app') {
+  window.location.assign('https://bkchu.github.io/confession-app/');
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
