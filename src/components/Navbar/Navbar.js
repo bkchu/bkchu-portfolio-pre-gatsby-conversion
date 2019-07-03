066456bc3cb5a3555b7c4e56ed03f8a5bdb77cb7
Brandon Chung <brandon.k.chung@gmail.com>
2019-07-03 16:43:44 -0500

    navbar revamp

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,60 +6,60 @@ import { contactModal } from '../../redux/ducks/reducer';
 import Logo from '../../assets/BKC_Logo.svg';
 
 class Navbar extends Component {
-    state = {
-        height: 0
-    };
-    componentDidMount() {
-        document.addEventListener('scroll', this.trackScrolling);
-    }
+  state = {
+    height: 0
+  };
+  componentDidMount() {
+    document.addEventListener('scroll', this.trackScrolling);
+  }
 
-    trackScrolling = () => {
-        this.setState({ height: window.scrollY });
-    };
+  trackScrolling = () => {
+    this.setState({ height: window.scrollY });
+  };
 
-    render() {
-        let navbarClasses = ['Navbar__nav'];
-        if (this.props.location.pathname === '/' && this.state.height >= 300) {
-            navbarClasses.push('Navbar__nav--scrolled');
-        } else if (
-            this.props.location.pathname !== '/' &&
-            this.state.height >= 75
-        ) {
-            navbarClasses.push('Navbar__nav--scrolled');
-        }
-        return (
-            <div className="Navbar">
-                <nav className={navbarClasses.join(' ')}>
-                    <Link to="/" className="Navbar__link link">
-                        <img className="Navbar__logo" src={Logo} alt="brand" />
-                    </Link>
-                    <div className="Navbar__link-list">
-                        <Link to="/" className="Navbar__link link">
-                            Home
-                        </Link>
-                        <Link to="/projects" className="Navbar__link link">
-                            Projects
-                        </Link>
-                        <Link to="/skills" className="Navbar__link link">
-                            Skills
-                        </Link>
-                        <Link to="/experience" className="Navbar__link link">
-                            Experience
-                        </Link>
-                        <a
-                            onClick={() => this.props.contactModal()}
-                            className="Navbar__link link"
-                        >
-                            Contact
-                        </a>
-                    </div>
-                </nav>
-            </div>
-        );
+  render() {
+    let navbarClasses = ['Navbar__nav'];
+    if (this.props.location.pathname === '/' && this.state.height >= 300) {
+      navbarClasses.push('Navbar__nav--scrolled');
+    } else if (
+      this.props.location.pathname !== '/' &&
+      this.state.height >= 75
+    ) {
+      navbarClasses.push('Navbar__nav--scrolled');
     }
+    return (
+      <div className="Navbar">
+        <nav className={navbarClasses.join(' ')}>
+          <Link to="/" className="Navbar__logo-link link">
+            <img className="Navbar__logo" src={Logo} alt="brand" />
+          </Link>
+          <div className="Navbar__link-list">
+            <Link to="/" className="Navbar__list-link link">
+              Home
+            </Link>
+            <Link to="/projects" className="Navbar__list-link link">
+              Projects
+            </Link>
+            <Link to="/skills" className="Navbar__list-link link">
+              Skills
+            </Link>
+            <Link to="/experience" className="Navbar__list-link link">
+              Experience
+            </Link>
+            <a
+              onClick={() => this.props.contactModal()}
+              className="Navbar__list-link link"
+            >
+              Contact
+            </a>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default connect(
-    null,
-    { contactModal }
+  null,
+  { contactModal }
 )(withRouter(Navbar));
