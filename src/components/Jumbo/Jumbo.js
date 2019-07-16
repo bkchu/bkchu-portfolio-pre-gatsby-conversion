5edd14bb9233d2f373287492a222a4414765b052
Brandon Chung <brandon.k.chung@gmail.com>
2019-07-16 16:28:52 -0500

    add framer-motion

--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Jumbo = () => {
+  const variants = {
+    hidden: {
+      x: -100,
+      opacity: 0
+    },
+    visible: {
+      x: 0,
+      opacity: 1
+    }
+  }
   return (
     <div className="Jumbo">
       <div className="container">
-        <h2 className="Jumbo__description">
-          <span className="Jumbo__name">I'm Brandon.</span> <br />
+        <motion.h2
+          initial="hidden"
+          animate="visible"
+          transition={{ type: 'spring', damping: 20, stiffness: 300, delay: 0.5 }}
+          // initial={false}
+          variants={variants}
+          className="Jumbo__description"
+        >
+          <span className="Jumbo__name">I'm Brandon.</span>
+          <br />
           I'm a UI Developer curating experiences that are pleasing and
           accessible.
-        </h2>
+        </motion.h2>
       </div>
     </div>
   );
