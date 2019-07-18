14dbc2baa677fa09e1933a80698be94cef52d67a
Brandon Chung <brandon.k.chung@gmail.com>
2019-07-17 19:38:45 -0500

    fix contact modal

--- src/components/Modal/ModalWrapper.js
+++ src/components/Modal/ModalWrapper.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { motion } from 'framer-motion';
 
 class ModalWrapper extends Component {
   componentDidMount() {
@@ -31,9 +32,28 @@ class ModalWrapper extends Component {
       </button>
     ) : null;
 
+    const modalVariants = {
+      open: {
+        y: 0,
+        opacity: 1,
+        transition: {
+          duration: 0.2
+        }
+      },
+      closed: {
+        y: 50,
+        opacity: 0
+      }
+    };
+
     return (
       <div className="Modal" onClick={handleBackgroundClick}>
-        <div className="Modal__content">
+        <motion.div
+          initial="closed"
+          animate="open"
+          variants={modalVariants}
+          className="Modal__content"
+        >
           <header className="Modal__header">
             <button
               className="Modal__button--hide"
@@ -45,7 +65,7 @@ class ModalWrapper extends Component {
           <div>{this.props.children}</div>
 
           {okButton}
-        </div>
+        </motion.div>
       </div>
     );
   }
